82b2b4a4691818fb6abb9696a77aa61ed5eda96e
NyshimoriMizuki@gmail.com <nyshimorikenji@gmail.com>
2023-01-12 14:34:46 -0300

    working word generator

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,5 @@ use wordgen::WordGenerator;
 fn main() {
     let setup = SetupCL::from_json("samples/test.scl.json").expect("File don't existe");
     let test = WordGenerator::new(&setup);
-    println!("{}", test.new_syllable(true));
+    println!("{:?}", test.generate(Some(5)));
 }
--- src/wordgen/mod.rs
+++ src/wordgen/mod.rs
@@ -19,18 +19,46 @@ impl WordGenerator<'_> {
             syllable_struct: break_into_vec(setup.get_syllable_struct()),
         }
     }
-    pub fn generate(self, num: Option<u32>) {}
+    pub fn generate(self, num: Option<u32>) -> Vec<String> {
+        let mut words: Vec<String> = Vec::new();
+        let mut rng = rand::thread_rng();
+
+        for _ in 0..num.unwrap_or(1) {
+            let num_of_syllables = rng.gen_range(0..self.max_word_length) + 1;
+            let stress_possition = rng.gen_range(0..num_of_syllables);
+
+            let mut new_word: Vec<String> = Vec::new();
+            for i in 0..num_of_syllables {
+                if i == stress_possition {
+                    new_word.push(self.new_syllable(true));
+                } else {
+                    new_word.push(self.new_syllable(false));
+                }
+            }
+            words.push(new_word.join("·"));
+        }
+
+        words
+    }
 
     pub fn new_syllable(&self, stress: bool) -> String {
         let mut new_syllable = String::new();
+        let mut rand_gen = rand::thread_rng();
 
         for i in &self.syllable_struct {
-            if i.contains("(") && rand::thread_rng().gen::<bool>() {
+            if i.contains("(") && rand_gen.gen::<bool>() {
                 continue;
             }
-            let group = if i.contains("(") { &i[1..2] } else { *i };
-            println!("{:?}", self.phonemes.get(group).expect("no phoneme"));
-            new_syllable.push_str(i);
+            let phonemes = self
+                .phonemes
+                .get(if i.contains("(") { &i[1..2] } else { *i })
+                .expect("no phoneme");
+
+            new_syllable.push_str(
+                phonemes
+                    .choose(&mut rand_gen)
+                    .expect("error on choice some phoneme"),
+            );
         }
         if stress {
             return format!("ˈ{}", new_syllable);
